e0fdf15f06c469e38cf285b620734ed2cd784876
Alexander Malikov <AEMalikov@mtsbank.ru>
2024-12-05 00:00:16 +0300

    init master

--- src/main/java/com/malikov/study/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.malikov.study.repository;
-
-import com.malikov.study.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
